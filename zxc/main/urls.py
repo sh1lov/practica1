c119f23d4f88d21e82e69cda443d0305e532501b
sh1lov <sh1lovandr@yandex.ru>
2022-12-28 18:36:22 +0300

    create all

--- zxc/main/urls.py
+++ zxc/main/urls.py
@@ -1,9 +1,16 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about-us', views.about, name='about'),
     path('catalog', views.catalog, name='catalog')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
